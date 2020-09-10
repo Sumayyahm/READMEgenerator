7a5155f0ffe218a82b87b060c9078950083e7612
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-09-09 22:02:10 -0500

    Updated index.js, added questions, function to initialize prog and function to write README file

---
+++ index.js
@@ -0,0 +1,76 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+      },
+      {
+        type: "input",
+        name: "Description",
+        message: "Write a description"
+      },
+      {
+        type: "input",
+        name: "Installation",
+        message: "Provide installation instructions"
+      },
+      {
+        type: "input",
+        name: "Usage",
+        message: "Describe the usage"
+      },
+
+      {
+        type: "checkbox",
+        message: "Choose your license",
+        name: "License",
+        choices : [
+            "MIT License",
+            "GPL 3.0",
+            "APACHE 2.0",
+            "MS-PL",
+            "WTFPL",
+            "None"
+        ]
+      },
+      {
+        type: "input",
+        name: "Author",
+        message: "Who is the author"
+      },
+      {
+        type: "input",
+        name: "Contact",
+        message: "How can a user contribute or report issues? Provide contact information"
+      }
+];
+
+// function to write README file
+function writeToFile(filename, data) {
+    fs.writeFile(filename, data, function(err){
+        if (err) {
+            return console.log(err);
+          }
+        
+          console.log("Successfully created a README.md");
+    });
+    
+}
+
+// function to initialize program
+function init() {
+    inquirer.prompt(questions)
+    .then(function(answers) {
+       
+    });
+
+
+}
+
+// function call to initialize program
+init();
